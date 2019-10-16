b131d1cbd500f56569ed8c2dff4142d600079341
Romain <romain.goussault@gmail.com>
2019-10-16 14:41:45 +0200

    Add scenario loop

--- scenario.py
+++ scenario.py
@@ -19,28 +19,31 @@ class Scenario:
     self.dataset_name = 'MNIST'
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     
+    # Add express option to limit the dataset size
+    
     # Truncate dataset for quicker debugging/testing
-    self.x_train = x_train[:600]
-    self.y_train = y_train[:600]
+    self.x_train = x_train
+    self.y_train = y_train
  
-    self.x_test = x_test[:300]
-    self.y_test = y_test[:300]
+    self.x_test = x_test
+    self.y_test = y_test
     
     # Define the desired number of independant nodes
     # Nodes mock different partners in a collaborative data science project
-    self.nodes_count = 2
+    self.nodes_count = 3
 
     # Configure the desired respective datasets sizes of the nodes
     # Should the nodes receive an equivalent amount of samples each...
     # ... or receive different amounts?
     # Define the percentages of samples per node
     # Sum has to equal 1 and number of items has to equal NODES_COUNT
-    self.amounts_per_node = [0.2, 0.8]
+    self.amounts_per_node = [0.33, 0.33, 0.34]
     
     # Configure if nodes get overlapping or distinct samples
     # Should the nodes receive data from distinct categories...
     # ... or just random samples?
-    self.overlap_or_distinct = 'Distinct' # Toggle between 'Overlap' and 'Distinct'
+    # TODO find better names
+    self.overlap_or_distinct = 'Overlap' # Toggle between 'Overlap' and 'Distinct'
     
     # Define if test data should be split between nodes...
     # ... or if each node should refer to the complete test set
@@ -77,7 +80,7 @@ class Scenario:
     
     now = datetime.datetime.now()
     now_str = now.strftime("%Y-%m-%d_%Hh%M")
-    target_filename = now_str + '.json'
+    target_filename = now_str + '.txt'
     target_file_path = os.path.join(target_folder, target_filename)
     
     with open(target_file_path, 'w', encoding='utf-8') as f:
--- simulation_run.py
+++ simulation_run.py
@@ -21,60 +21,75 @@ from timeit import default_timer as timer
 import numpy as np
 
 
-#%% Fetch data splitting scenario
+#%% Create scenarii
 
-my_basic_scenario = scenario.Scenario()
-node_list = data_splitting.process_data_splitting_scenario(my_basic_scenario)
+my_balanced_scenario = scenario.Scenario()
+my_unbalanced_scenario = scenario.Scenario()
+my_unbalanced_scenario.amounts_per_node = [0.05, 0.15, 0.8]
 
-
-#%% Preprocess data for compatibility with keras CNN models
-
-preprocessed_node_list = fl_training.preprocess_node_list(node_list)
-
-
-#%% Train and eval on all nodes according to scenario
-
-fl_score = fl_training.compute_test_score(preprocessed_node_list)
+scenarii_list = []
+scenarii_list.append(my_balanced_scenario)
+scenarii_list.append(my_unbalanced_scenario)
 
 
-#%% Contributivity 1: Baseline contributivity measurement (Shapley Value)
+#%% Run
 
-shapley_contrib = contributivity.Contributivity('Shapley values')
-
-start = timer()
-shapley_contrib.contributivity_scores = contributivity_measures.compute_SV(preprocessed_node_list)
-end = timer()
-
-shapley_contrib.computation_time = np.round(end - start)
-
-my_basic_scenario.append_contributivity(shapley_contrib)
-print(shapley_contrib)
-
-      
-#%% Contributivity 1 : performance scores of models trained independently on each node
-
-independant_raw_contrib = contributivity.Contributivity('Independant scores raw')
-independant_additiv_contrib = contributivity.Contributivity('Independant scores additiv')
-
-start = timer()
-scores = contributivity_measures.compute_independent_scores(preprocessed_node_list, fl_score)
-end = timer()
-
-independant_computation_time = np.round(end - start)
-independant_raw_contrib.computation_time = independant_computation_time
-independant_additiv_contrib.computation_time = independant_computation_time
-
-# TODO use dict instead of 0/1 indexes
-independant_raw_contrib.contributivity_scores = scores[0]
-independant_additiv_contrib.contributivity_scores = scores[1]
-
-my_basic_scenario.append_contributivity(independant_raw_contrib)
-my_basic_scenario.append_contributivity(independant_additiv_contrib)
-print(independant_raw_contrib)
-print('')
-print(independant_additiv_contrib)
-
-      
-#%% Save results to file
-
-my_basic_scenario.to_file()
+for current_scenario in scenarii_list:
+    
+    #%% Fetch data splitting scenario
+    
+    
+    node_list = data_splitting.process_data_splitting_scenario(current_scenario)
+    
+    
+    #%% Preprocess data for compatibility with keras CNN models
+    
+    preprocessed_node_list = fl_training.preprocess_node_list(node_list)
+    
+    
+    #%% Train and eval on all nodes according to scenario
+    
+    fl_score = fl_training.compute_test_score(preprocessed_node_list)
+    
+    
+    #%% Contributivity 1: Baseline contributivity measurement (Shapley Value)
+    
+    shapley_contrib = contributivity.Contributivity('Shapley values')
+    
+    start = timer()
+    shapley_contrib.contributivity_scores = contributivity_measures.compute_SV(preprocessed_node_list)
+    end = timer()
+    
+    shapley_contrib.computation_time = np.round(end - start)
+    
+    current_scenario.append_contributivity(shapley_contrib)
+    print(shapley_contrib)
+    
+          
+    #%% Contributivity 1 : performance scores of models trained independently on each node
+    
+    independant_raw_contrib = contributivity.Contributivity('Independant scores raw')
+    independant_additiv_contrib = contributivity.Contributivity('Independant scores additiv')
+    
+    start = timer()
+    scores = contributivity_measures.compute_independent_scores(preprocessed_node_list, fl_score)
+    end = timer()
+    
+    independant_computation_time = np.round(end - start)
+    independant_raw_contrib.computation_time = independant_computation_time
+    independant_additiv_contrib.computation_time = independant_computation_time
+    
+    # TODO use dict instead of 0/1 indexes
+    independant_raw_contrib.contributivity_scores = scores[0]
+    independant_additiv_contrib.contributivity_scores = scores[1]
+    
+    current_scenario.append_contributivity(independant_raw_contrib)
+    current_scenario.append_contributivity(independant_additiv_contrib)
+    print(independant_raw_contrib)
+    print('')
+    print(independant_additiv_contrib)
+    
+          
+    #%% Save results to file
+    
+    current_scenario.to_file()
