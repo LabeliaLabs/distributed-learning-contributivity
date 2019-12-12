34722614c09d5745b673cc95130d97dc1bc25fbc
Thomas Galtier <57995084+Thomas-Galtier@users.noreply.github.com>
2019-12-12 12:10:40 +0100

    Improve variance computation, simplify while loop conditions, use sort built-in function
    The variances of the estimations of the shapley values are now computed using the maximum likelihood estimator -- not the moment estimator as before. It now uses a little more memory but it has the advantage of always yielding a positive estimation of the variance.
    
    This commit removed the condition to ensure the first 50 permutations  were different in the while loop of the truncated_MC method. Have the  50 permutations equal is an extremely unlikely event, this condition was therefore unnecessary.

--- truncated_MonteCarlo.py
+++ truncated_MonteCarlo.py
@@ -11,83 +11,85 @@ import scenario
 from scipy.stats import norm
 
 
-def truncated_MC(preprocessed_node_list, sv_accuracy=0.001, contrib_accuracy=0.01):
+def truncated_MC(preprocessed_node_list,characteristic_func, sv_accuracy=0.001, alpha=0.95, contrib_accuracy=0.01):
     """Return the vector of approximated shapeley value corresponding to a list of node and a characteristic function using the truncated monte-carlo method."""
-    
-    characteristic_func=fl_training.compute_test_score
+ 
     n = len(preprocessed_node_list)
     
-    # we store the value of the characteristic function in order to avoid recomputing it twice
+    # We store the value of the characteristic function in order to avoid recomputing it twice
     char_value_dict={():0} # the dictionary that will countain the values
     listn=np.arange(n)
     
     # Return the characteristic function of the nodelist associated to the ensemble permut, without recomputing it if it was already computed
     def not_twice_characteristic(permut):
-        #sort permut
-        isin=np.repeat(False,n)
-        isin[permut]=True
-        permut=listn[isin]
-        try: #return the characteristic_func(permut) if it was already computed
+        # Sort permut
+        permut=np.sort(permut)
+        try: # Return the characteristic_func(permut) if it was already computed
             return char_value_dict[tuple(permut)]
-        except KeyError: # characteristic_func(permut) has not been computed yet, so we compute, store, and return characteristic_func(permut)
-            small_node_list = [preprocessed_node_list[i] for i in permut]
+        except KeyError: # Characteristic_func(permut) has not been computed yet, so we compute, store, and return characteristic_func(permut)
+            small_node_list = np.array([preprocessed_node_list[i] for i in permut])
             char_value_dict[tuple(permut)]= characteristic_func(small_node_list)
             return char_value_dict[tuple(permut)]
 
         
-    # truncated_MC initialisation
+    characteristic_all_node= not_twice_characteristic(np.arange(n)) # Characteristic function on all nodes
     if n==1:
-        return np.array([1])
-    else:
-        characteristic_all_node= not_twice_characteristic(np.arange(n)) #characteristic function on all nodes
-        sv = np.zeros(n) #store current estimation of the shapley values 
-        mean_squares_sv = np.zeros(n) # used for variance computation in the condition of the while loop below 
-        permutation=np.zeros(n) # store the current permutation 
-        firsts_are_equal =True
+        return({'sv': characteristic_all_node,'std_vs': np.array([0]),'prop': np.array([1]), 'computed_val':char_value_dict})
+    else: 
+        contributions=np.array([[]])
+        permutation=np.zeros(n) # Store the current permutation
         t=0
-        while t<50 or t<=norm.ppf(0.975, loc=0, scale=1)**2* np.max(mean_squares_sv-sv**2) /sv_accuracy**2 or firsts_are_equal: #check if the length of the confidence interval  is below the value of sv_accuracy
+        q=norm.ppf((1-alpha)/2, loc=0, scale=1)
+        v_max=0
+        while t<100 or t<q**2 *v_max  /(sv_accuracy*characteristic_all_node)**2 : # Check if the length of the confidence interval  is below the value of sv_accuracy*characteristic_all_node
             t+=1
-            print(t<50,t<=norm.ppf(0.975, loc=0, scale=1)**2* np.max(mean_squares_sv-sv**2) /sv_accuracy**2,firsts_are_equal)
-            print(t)
-            print(sv)
-            previous_sv = np.copy(sv)#store previous estimation of the shapley values
-            previous_mean_square_sv= np.copy(mean_squares_sv) #store previous value of   the mean_square_sv
-            previous_permutation=np.copy(permutation)# store the previous permutation
-            permutation = np.random.permutation(n) # store the current permutation
-            if firsts_are_equal and t>1:# makes sure we don't stop the loop because the first permutations are the same (very unlikely but can still happen if n is small)
-                firsts_are_equal=   (permutation==previous_permutation).all()
-            char_nodelists = np.zeros(n+1) # store the characteristic function on each ensemble built with the first elements of the permutation
+            
+            if t==1:
+                contributions=np.array([np.zeros(n)])
+            else:
+                contributions=np.vstack((contributions, np.zeros(n)))
+            
+            permutation = np.random.permutation(n) # Store the current permutation 
+            char_nodelists = np.zeros(n+1) # Store the characteristic function on each ensemble built with the first elements of the permutation
             char_nodelists[-1]=characteristic_all_node
-            for j in range(n):  # iteration on sv[permutation[j]]
+            for j in range(n):
                 if abs(characteristic_all_node-char_nodelists[j])<contrib_accuracy :
                     char_nodelists[j+1] = char_nodelists[j]
                 else:
                     char_nodelists[j+1] = not_twice_characteristic(permutation[:j+1])
-                sv[permutation[j]]              = (t-1)/t*previous_sv[permutation[j]]            +(char_nodelists[j+1]-char_nodelists[j])/t
-                mean_squares_sv[permutation[j]] = (t-1)/t*previous_mean_square_sv[permutation[j]]+(char_nodelists[j+1]-char_nodelists[j])**2/t
-    return({'sv':sv,'std': np.sqrt((mean_squares_sv-sv**2)/t), 'all_val':char_value_dict})
+                contributions[-1][permutation[j]]  =  char_nodelists[j+1]-char_nodelists[j]
+            v_max=np.max(np.var(contributions,axis=0))
+        sv=np.mean(contributions,axis=0)
+    return({'sv':sv,'std_vs': np.std(contributions,axis=0),'prop':sv/np.sum(sv), 'computed_val':char_value_dict})
+
         
                 
-# TEST:        
-#import data_splitting
-#import fl_training
-#my_custom_scenario = scenario.Scenario()
-#my_custom_scenario.nodes_count=5
-#my_custom_scenario.amounts_per_node = [1/my_custom_scenario.nodes_count for i in range ((my_custom_scenario.nodes_count))]
-#my_custom_scenario.samples_split_option = 'Random' # or 'Stratified'
-#my_custom_scenario.testset_option = 'Centralised' # Toggle between 'Centralised' and 'Distributed'
-#my_custom_scenario.x_train = my_custom_scenario.x_train[:5000] # Truncate dataset if needed for quicker debugging/testing
-#my_custom_scenario.y_train = my_custom_scenario.y_train[:5000] # Truncate dataset if needed for quicker debugging/testing
-#my_custom_scenario.x_test = my_custom_scenario.x_test[:1000] # Truncate dataset if needed for quicker debugging/testing
-#my_custom_scenario.y_test = my_custom_scenario.y_test[:1000] # Truncate dataset if needed for quicker debugging/testing
-#
-#node_list = data_splitting.process_data_splitting_scenario(my_custom_scenario)
-#preprocessed_node_list = fl_training.preprocess_node_list(node_list)
-#res1=truncated_MC(preprocessed_node_list, sv_accuracy=0.005, contrib_accuracy=0.001)
-#
-#char_value_list=[   res1['all_val'][tuple(i)]  for r in range(1,my_custom_scenario.nodes_count+1) for i in combinations(range(my_custom_scenario.nodes_count),r)]
-#res1_true=main(my_custom_scenario.nodes_count,char_value_list)
-#print(abs(res1['sv']-res1_true) <0.005)#vrai 95% du temps
+# # TEST:        
+# import data_splitting
+# import fl_training
+# import constants
+# import itertools
+# import shapley_value.shapley as shap
+# constants.NB_EPOCHS=5
+# constants.BATCH_SIZE=1000
+# my_custom_scenario = scenario.Scenario()
+# my_custom_scenario.nodes_count=5
+# my_custom_scenario.amounts_per_node = [1/my_custom_scenario.nodes_count for i in range ((my_custom_scenario.nodes_count))]
+# my_custom_scenario.samples_split_option = 'Random' # or 'Stratified'
+# my_custom_scenario.testset_option = 'Centralised' # Toggle between 'Centralised' and 'Distributed'
+# my_custom_scenario.x_train = my_custom_scenario.x_train[:5000] # Truncate dataset if needed for quicker debugging/testing
+# my_custom_scenario.y_train = my_custom_scenario.y_train[:5000] # Truncate dataset if needed for quicker debugging/testing
+# my_custom_scenario.x_test = my_custom_scenario.x_test[:1000] # Truncate dataset if needed for quicker debugging/testing
+# my_custom_scenario.y_test = my_custom_scenario.y_test[:1000] # Truncate dataset if needed for quicker debugging/testing
+
+# node_list = data_splitting.process_data_splitting_scenario(my_custom_scenario)
+# preprocessed_node_list = fl_training.preprocess_node_list(node_list)
+# sv_accuracy,alpha=0.005,0.95
+# res1=truncated_MC(preprocessed_node_list,characteristic_func= fl_training.compute_test_score, sv_accuracy=sv_accuracy, alpha=alpha, contrib_accuracy=0.001)
+
+# char_value_list=[   res1['computed_val'][tuple(i)]  for r in range(1,my_custom_scenario.nodes_count+1) for i in itertools.combinations(range(my_custom_scenario.nodes_count),r)]
+# res1_true=shap.main(my_custom_scenario.nodes_count,char_value_list)
+# print(abs(res1['sv']-res1_true) <sv_accuracy)#vrai (1-alpha)% du temps
 
 
 
