df68dcf1a0c41aa1e1fe358410a8e76788d9ef46
bowni <bowni@pm.me>
2020-03-31 13:09:08 +0200

    Fix case where minibatch_count is 1

--- scenario.py
+++ scenario.py
@@ -74,6 +74,7 @@ class Scenario:
         self.epoch_count = 40
         if 'minibatch_count' in params.keys():
             self.minibatch_count = params['minibatch_count']
+            assert self.minibatch_count > 0
         else:
             self.minibatch_count = 20
 
@@ -233,6 +234,9 @@ class Scenario:
 
         # Check coherence of node_list versus nodes_count
         assert len(self.node_list) == self.nodes_count
+
+        # Check coherence of number of mini-batches versus smaller node
+        assert self.minibatch_count <= (min(self.amounts_per_node) * len(x_train))
 
         # Print and plot for controlling
         print("\n### Splitting data among nodes:")
