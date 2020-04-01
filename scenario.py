d8d4c4cc6acef94d5ad3e496ae7bf15bb29e0547
Romain Goussault <romain.goussault@gmail.com>
2020-04-01 11:41:25 +0200

    Add federated_computation_time in scenario

--- scenario.py
+++ scenario.py
@@ -61,6 +61,8 @@ class Scenario:
         # Performance of the model trained in a distributed way on all nodes
         self.federated_test_score = int
 
+        self.federated_computation_time = int
+
         # Define how federated learning aggregation steps are weighted. Toggle between 'uniform' and 'data_volume'
         # Default is 'uniform'
         if "aggregation_weighting" in params:
@@ -338,6 +340,7 @@ class Scenario:
                 dict_results["epoch_count"] = self.epoch_count
                 dict_results["is_early_stopping"] = self.is_early_stopping
                 dict_results["federated_test_score"] = self.federated_test_score
+                dict_results["federated_computation_time"] = self.federated_computation_time
                 dict_results["scenario_name"] = self.scenario_name
                 dict_results["short_scenario_name"] = self.short_scenario_name
                 dict_results["minibatch_count"] = self.minibatch_count
