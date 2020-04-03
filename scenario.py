b5852a88efaf38f3bdb7f9a42852f8e8b1ad7dd4
Thomas <tgaltier@gmail.com>
2020-04-03 15:13:39 +0200

    only run the default list when no method provided in config file

--- scenario.py
+++ scenario.py
@@ -89,13 +89,20 @@ class Scenario:
             assert self.minibatch_count > 0
         else:
             self.minibatch_count = 20
-        
+            
         # Contributivity methods
+        known_methods_list = [
+            "Shapley values",
+            "Independant scores", 
+            "TMCS",
+            ]
+        
+        # List of Contributivity methods runned by default if no method was given in the config file 
         methods_default = [
             "Shapley values",
             "Independant scores",
             "TMCS values"
-        ]
+            ]
         
         self.methods = []
         if not 'methods' in params:
@@ -103,13 +110,15 @@ class Scenario:
         else:
             if params['methods']:
                 for el in params['methods']:
-                    if el in methods_default:
+                    if el in known_methods_list:
                         self.methods.append(el)
                     else:
                         raise Exception('method ' + el + ' is not in methods list.')
             else:
-                raise Exception("No contributivity method given in config file")
-        
+                raise Exception("No contributivity method given in config file")      
+                
+                
+                
         # Early stopping stops ML training when performance increase is not significant anymore
         # It is used to optimize the number of epochs and the execution time
         self.is_early_stopping = True # Toggle between True and False
