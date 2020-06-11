1b8cc8b03a32577f00c4a3cbf54cdfeef0c43291
bowni <bowni@pm.me>
2020-06-11 19:27:37 +0200

    Remove unused constants

--- constants.py
+++ constants.py
@@ -3,16 +3,6 @@
 Declaration of constants.
 """
 
-# Dataset
-DATASET = str
-
-# Data constants
-NUM_CLASSES = 10
-
-# input image dimensions
-IMG_ROWS, IMG_COLS = 28, 28
-INPUT_SHAPE = (IMG_ROWS, IMG_COLS, 1)
-
 # ML constants
 DEFAULT_BATCH_SIZE = 256
 MAX_BATCH_SIZE = 2**20
