978d9334ab693b19166f69978f9a61fe9d68bfee
Gauthier <59366273+GauthierGar@users.noreply.github.com>
2020-07-03 17:30:43 +0200

    Update dataset_imdb.py

--- datasets/dataset_imdb.py
+++ datasets/dataset_imdb.py
@@ -9,19 +9,30 @@ from keras.models import Sequential
 from keras import layers
 import keras
 
-# Load Data
-(x_train, y_train),(x_test, y_test)=tf.keras.datasets.imdb.load_data(
-    path='imdb.npz', num_words=None, skip_top=50, maxlen=None,
-    seed=113, start_char=1, oov_char=2, index_from=3)
-
-input_shape = tf.keras.Input(shape=(None,), dtype="int64")
-
-# 2 Classes: review positive or negative
-num_classes = 2
 
 def preprocess_dataset_labels(y):
     y = tf.keras.utils.to_categorical(y, num_classes)
     return y
+
+# Padd with the out_of_vocabulary char=2 ==> imdb.load_data()
+def preprocess_dataset_inputs(x):
+
+    for i in range(len(x)):
+        x[i] = x[i] + [2] * (dimension-len(x[i]))
+
+    return np.asarray(x)
+
+# Load Data
+(x_train, y_train),(x_test, y_test)=tf.keras.datasets.imdb.load_data(num_words=10000)
+
+input_shape = tf.keras.Input(shape=(None,), dtype="int64")
+
+dimension = 10000 # The longest review is nearly 3000 words long
+
+# 2 Classes: review positive or negative
+num_classes = 2
+
+
 
 max_features = 20000
 embedding_dim = 128
@@ -51,7 +62,7 @@ def generate_new_model_for_dataset():
     # model.add(Dense(1, activation="sigmoid"))
 
     # Input - Layer
-    model.add(layers.Dense(50, activation = "relu", input_shape=input_shape))
+    model.add(layers.Dense(50, activation = "relu", input_shape=(10000,)))
     # Hidden - Layers
     model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
     model.add(layers.Dense(50, activation = "relu"))
