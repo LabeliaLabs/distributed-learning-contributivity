3caaae036c131213d929817619e058af4feb025b
Gauthier <59366273+GauthierGar@users.noreply.github.com>
2020-07-06 17:09:06 +0200

    Update dataset_imdb.py

--- datasets/dataset_imdb.py
+++ datasets/dataset_imdb.py
@@ -2,67 +2,54 @@
 
 import tensorflow as tf
 import numpy as np
-from keras.datasets import imdb
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import Conv1D, GlobalMaxPool1D, Embedding
-from keras.models import Sequential
+from tensorflow import keras
+from keras.utils import to_categorical
+from keras import models
 from keras import layers
-import keras
+from keras.datasets import imdb
 
+(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=10000)
+
+data = np.concatenate((training_data, testing_data), axis=0)
+
+targets = np.concatenate((training_targets, testing_targets), axis=0)
+
+def preprocess_dataset_inputs(sequences, dimension = 10000):
+    results = np.zeros((len(sequences), dimension))
+    for i in range(len(sequences)):
+        j = 0
+        while j < len(sequences[i]):
+            results[i][j] = sequences[i][j]
+            j = j + 1
+    return results
 
 def preprocess_dataset_labels(y):
-    y = tf.keras.utils.to_categorical(y, num_classes)
+    y = keras.utils.to_categorical(y, num_classes)
     return y
 
-# Padd with the out_of_vocabulary char=2 ==> imdb.load_data()
-def preprocess_dataset_inputs(x):
-
-    for i in range(len(x)):
-        x[i] = x[i] + [2] * (dimension-len(x[i]))
-
-    return np.asarray(x)
-
-# Load Data
-(x_train, y_train),(x_test, y_test)=tf.keras.datasets.imdb.load_data(num_words=10000)
-
-input_shape = tf.keras.Input(shape=(None,), dtype="int64")
-
-dimension = 10000 # The longest review is nearly 3000 words long
-
-# 2 Classes: review positive or negative
+#input_shape = keras.Input(shape=(25000,10000), dtype="int64")
+input_shape = (25000,10000)
 num_classes = 2
+data = preprocess_dataset_inputs(data)
 
+targets = np.array(targets).astype("int64")
 
+x_test = data[:25000]
+y_test = targets[:25000]
+x_train = data[25000:]
+y_train = targets[25000:]
 
-max_features = 20000
-embedding_dim = 128
-sequence_length = 500
+print("x_train:", x_train[0:2])
+print("x_train shape:", x_train.shape)
+print("y_train:", x_train[0:2])
+print("y_train shape:", y_train.shape)
 
-# Model structure and generation
 def generate_new_model_for_dataset():
-    """ Return a CNN model from scratch based on given batch_size"""
 
-    # Sequential groups a linear stack of layer into a tf.kera.Model
-    model = Sequential() # Instancie un objet de type Sequential
-
-    # Next, we add a layer to map those vocab indices into a 
-    # space of dimensionality 'embedding_dim'.
-    #model.add(Embedding(max_features, embedding_dim))
-    #model.add(Dropout(0.5))
-
-    # Conv1D + global max pooling
-    #model.add(Conv1D(128, 7, padding="valid", activation="relu", strides=3))
-    #model.add(Conv1D(128, 7, padding="valid", activation="relu", strides=3))
-    #model.add(GlobalMaxPool1D())
-
-    # We add a vanilla hidden layer:
-    #model.add(Dense(128, activation="relu"))
-    #model.add(Dropout(0.5))
-
-    # model.add(Dense(1, activation="sigmoid"))
+    model = models.Sequential()
 
     # Input - Layer
-    model.add(layers.Dense(50, activation = "relu", input_shape=(10000,)))
+    model.add(layers.Dense(50, activation = "relu", input_shape=input_shape))
     # Hidden - Layers
     model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
     model.add(layers.Dense(50, activation = "relu"))
@@ -70,9 +57,13 @@ def generate_new_model_for_dataset():
     model.add(layers.Dense(50, activation = "relu"))
     # Output- Layer
     model.add(layers.Dense(1, activation = "sigmoid"))
+    model.summary()
 
-    model.compile(loss=keras.losses.categorical_crossentropy,
-            optimizer="adam",
-            metrics=['accuracy'])
+
+    model.compile(
+    optimizer = "adam",
+    loss = "categorical_crossentropy",
+    metrics = ["accuracy"]
+    )
 
     return model
