8c0e9f1e47208e7dc4af6ef3e2af80fc8447f4c1
gauthier.garioud@eleves.ec-nantes.fr <gauthier.garioud@eleves.ec-nantes.fr>
2020-07-07 11:17:59 +0200

    Add Flatten layer, change preprocess of labels and data

--- datasets/dataset_imdb.py
+++ datasets/dataset_imdb.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+IMDB dataset.
+inspired from:
+https://keras.io/examples/nlp/text_classification_from_scratch/
+https://builtin.com/data-science/how-build-neural-network-keras
+"""
 
 import tensorflow as tf
 import numpy as np
@@ -8,62 +14,59 @@ from keras import models
 from keras import layers
 from keras.datasets import imdb
 
-(training_data, training_targets), (testing_data, testing_targets) = imdb.load_data(num_words=10000)
+nb_words = 3000
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=nb_words)
+# Argument num_words => only the num_words first more frequent words are used
+# The others are replaced by 2
+# A Review start by the number 1
+# The longest review is less than 3000 words long
 
-data = np.concatenate((training_data, testing_data), axis=0)
+def preprocess_dataset_inputs(data):
+    data_list = []
+    for sample in data:
+        sample += [2] * (nb_words - len(sample)) # 2 is the over_of_vocabulary number
+        data_list.append(sample)
 
-targets = np.concatenate((training_targets, testing_targets), axis=0)
+    data_array = np.array(data_list)
+    data_array = data_array.reshape(data_array.shape[0], data_array.shape[1], 1)
 
-def preprocess_dataset_inputs(sequences, dimension = 10000):
-    results = np.zeros((len(sequences), dimension))
-    for i in range(len(sequences)):
-        j = 0
-        while j < len(sequences[i]):
-            results[i][j] = sequences[i][j]
-            j = j + 1
-    return results
+    return data_array
 
-def preprocess_dataset_labels(y):
+def preprocess_dataset_labels(y): # Do not preprocess labels here! => it's done in another script
     y = keras.utils.to_categorical(y, num_classes)
+    y = y.reshape(y.shape[0], y.shape[1])
+
     return y
 
-#input_shape = keras.Input(shape=(25000,10000), dtype="int64")
-input_shape = (25000,10000)
+# Padding
+nb_words = 3000
+
+input_shape = (nb_words,1)
+
+# Number of labels
 num_classes = 2
-data = preprocess_dataset_inputs(data)
 
-targets = np.array(targets).astype("int64")
-
-x_test = data[:25000]
-y_test = targets[:25000]
-x_train = data[25000:]
-y_train = targets[25000:]
-
-print("x_train:", x_train[0:2])
-print("x_train shape:", x_train.shape)
-print("y_train:", x_train[0:2])
-print("y_train shape:", y_train.shape)
+x_train = preprocess_dataset_inputs(x_train)
+x_test = preprocess_dataset_inputs(x_test)
 
 def generate_new_model_for_dataset():
-
     model = models.Sequential()
 
     # Input - Layer
     model.add(layers.Dense(50, activation = "relu", input_shape=input_shape))
+
     # Hidden - Layers
     model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
     model.add(layers.Dense(50, activation = "relu"))
     model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
     model.add(layers.Dense(50, activation = "relu"))
+    model.add(layers.Flatten()) # Adapt shape of output
+
     # Output- Layer
-    model.add(layers.Dense(1, activation = "sigmoid"))
-    model.summary()
+    model.add(layers.Dense(2, activation = "sigmoid")) # 2 => One-Hot Encoding!! with categorical_crossentropy
 
+    # model.summary() # Debugging
 
-    model.compile(
-    optimizer = "adam",
-    loss = "categorical_crossentropy",
-    metrics = ["accuracy"]
-    )
+    model.compile(optimizer = "adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
 
     return model
