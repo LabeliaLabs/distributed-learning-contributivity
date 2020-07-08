2aae51711fa9d8fbb71cb65e07b1524c43033e23
Gauthier <59366273+GauthierGar@users.noreply.github.com>
2020-07-08 17:32:33 +0200

    Update dataset_imdb.py
    Complete change of model: https://keras.io/examples/nlp/bidirectional_lstm_imdb/
    Use keras.preprocessing.sequence.pad_sequences() to preprocess easily the inputs and only keep 200 words per review
    Need for a categorical_crossentropy loss => multi_partner_learning.py ligne 440

--- datasets/dataset_imdb.py
+++ datasets/dataset_imdb.py
@@ -15,38 +15,29 @@ from keras import layers
 from keras.datasets import imdb
 
 out_of_vocabulary = 2
-max_nb_words_configured = 2500 # For padding
+# For padding
+max_nb_words_configured = 2500 
+# We keep the 10000 first more frequent words in the datasets
+# Also the dimentionality of the input of the Embedding layer
+size_vocabulary = 10000
+# We only keep the 200 first words of the reviews
+maxlen = 200
+# Number of labels
+num_classes = 2
 
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=10000, skip_top=35, start_char=1, oov_char=out_of_vocabulary, index_from=3)
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=size_vocabulary, skip_top=35, start_char=1, oov_char=out_of_vocabulary, index_from=3)
 
-# Argument num_words => only the num_words first more frequent words are used
+# We skip the 35 most frequent words
 # The others are replaced by 2
 # A Review start by the number 1
-# The longest review is less than 2500 words long
-
-def preprocess_dataset_inputs(data):
-    data_list = []
-    
-    assert max_nb_words_configured >= max([len(x) for x in data]),"max_nb_words_configured lower than the longest review"
-    
-    for sample in data:
-        sample += [out_of_vocabulary] * (max_nb_words_configured - len(sample)) # 2 is the over_of_vocabulary number
-        data_list.append(sample)
-
-    data_array = np.array(data_list)
-    data_array = data_array.reshape(data_array.shape[0], data_array.shape[1], 1)
-
-    return data_array
 
 def preprocess_dataset_labels(y): # Do not preprocess labels here! => it's done in another script
     y = keras.utils.to_categorical(y, num_classes)
-
     return y
 
-input_shape = (max_nb_words_configured,1)
-
-# Number of labels
-num_classes = 2
+# Standardize size of review at maxlen words: pre-padding (with 0) and pre-truncating
+def preprocess_dataset_inputs(x):
+    return keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
 
 x_train = preprocess_dataset_inputs(x_train)
 x_test = preprocess_dataset_inputs(x_test)
@@ -54,20 +45,17 @@ x_test = preprocess_dataset_inputs(x_test)
 def generate_new_model_for_dataset():
     model = models.Sequential()
 
-    # Input - Layer
-    model.add(layers.Dense(50, activation = "relu", input_shape=input_shape))
+    # Embed each integer in a 128-dimensional vector
+    inputs = keras.Input(shape=(None,), dtype="int32")
 
-    # Hidden - Layers
-    model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
-    model.add(layers.Dense(50, activation = "relu"))
-    model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
-    model.add(layers.Dense(50, activation = "relu"))
-    model.add(layers.Flatten()) # Adapt shape of output
+    model = models.Sequential()
+    model.add(layers.Embedding(size_vocabulary, 128))
+    model.add(layers.Bidirectional(layers.LSTM(64, return_sequences=True)))
+    model.add(layers.Bidirectional(layers.LSTM(64)))
 
-    # Output- Layer
-    model.add(layers.Dense(2, activation = "softmax")) # 2 => One-Hot Encoding!! with categorical_crossentropy
+    model.add(layers.Dense(2, activation="softmax"))
 
-    # model.summary() # Debugging
+    #model.summary() # Debugging
 
     model.compile(optimizer = "adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
 
