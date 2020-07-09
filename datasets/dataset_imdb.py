8e6acb70fadf1a82cf5874d10f73bbd6122d992a
Gauthier <59366273+GauthierGar@users.noreply.github.com>
2020-07-09 17:04:46 +0200

    Update dataset_imdb.py
    readibility and documentation

--- datasets/dataset_imdb.py
+++ datasets/dataset_imdb.py
@@ -11,23 +11,18 @@ from keras.preprocessing.sequence import pad_sequences
 from keras import models, layers
 from keras.datasets import imdb
 
+# Definition of dataset-specific parameters 
+maxlen = 200 # We only keep the 200 first words of the reviews
 out_of_vocabulary = 2
-# We keep the 10000 first more frequent words in the datasets
-# Also the dimentionality of the input of the Embedding layer
-size_vocabulary = 20000
-# We only keep the 200 first words of the reviews
-maxlen = 200
-# Number of labels
-num_classes = 2
+num_classes = 2 # 2 differents labels 
+skip_top_review = 35 # We skip the 35 most frequent words
+size_vocabulary = 20000 # We keep the 20000 first more frequent words in the datasets - Also the dimentionality of the input of the Embedding layer
+# The others are replaced by out_of_vocabulary - A Review start by the number 1 (start_char)
 
-# Not used here, but important another module
+# Not used here, but important in another module
 input_shape = keras.Input(shape=(None,), dtype="int32")
 
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=size_vocabulary, skip_top=35, start_char=1, oov_char=out_of_vocabulary, index_from=3)
-
-# We skip the 35 most frequent words
-# The others are replaced by 2
-# A Review start by the number 1
 
 def preprocess_dataset_labels(y): # Do not preprocess labels here! => it's done in another script
     y = to_categorical(y, num_classes)
@@ -43,11 +38,8 @@ x_test = preprocess_dataset_inputs(x_test)
 def generate_new_model_for_dataset():
     model = models.Sequential()
 
-    # Embed each integer in a 128-dimensional vector
-    inputs = keras.Input(shape=(None,), dtype="int32")
-
     model = models.Sequential()
-    model.add(layers.Embedding(size_vocabulary, 128))
+    model.add(layers.Embedding(size_vocabulary, 128)) # Embed each integer in a 128-dimensional vector
     model.add(layers.Bidirectional(layers.LSTM(64, return_sequences=True)))
     model.add(layers.Bidirectional(layers.LSTM(64)))
 
