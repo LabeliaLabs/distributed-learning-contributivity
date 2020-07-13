e72272d96f88621def9ae4eba522b19b004fbc96
natct10 <nathanael.cretin@substra.org>
2020-07-13 18:25:56 +0200

    Demo tests

--- tests.py
+++ tests.py
@@ -1,18 +1,14 @@
 # Pytest doc: https://docs.pytest.org/en/latest/getting-started.html#create-your-first-test
+# pytest tests.py
+# pytest -k TestDemoClass tests.py
+# pytest -k "test_ok" tests.py
+# pytest tests.py --pdb
 
-# Usage:
-# pytest -k "METHOD" FILE.py 
-# pytest -k CLASS FILE.py
-
-# Pdb option: run into the debugger if/when a test is failing
-# pytest FILE.py --pdb
-
-import pytest
 import utils
 import yaml
 
 
-class TestClass:
+class TestDemoClass:
 
     def test_ok(self):
         """
@@ -21,7 +17,6 @@ class TestClass:
         ok = "ok"
         assert "ok" in ok
 
-
     def test_ko(self):
         """
         Demo test 2
@@ -29,37 +24,21 @@ class TestClass:
         ko = "ko"
         assert "ok" not in ko
 
-
     def test_load_cfg(self):
         """
-        Test that the two config files are present
+        Check if the two config files are present
         and loaded with the load_cfg method
         """
         config_file = utils.load_cfg("config.yml")
         config_quick_debug_file = utils.load_cfg("config_quick_debug.yml")
         assert config_file and config_quick_debug_file
 
-
-    def test_config_files(self):
+    def test_load_config_files(self):
+        """
+        Check if the two config files are present
+        and loaded with the load method
+        """
         with open("config.yml", "r") as config_file:
             assert yaml.load(config_file, Loader=yaml.FullLoader)
         with open("config_quick_debug.yml", "r") as config_quick_debug_file:
             assert yaml.load(config_quick_debug_file, Loader=yaml.FullLoader)
-    
-
-    def test_config(self):
-        config = utils.load_cfg("config.yml")
-        assert config["experiment_name"] and config["n_repeats"] and config["scenario_params_list"]
-
-
-    @pytest.mark.parametrize('expected', [
-        ("my_example"),
-        ])
-    def test_eval(self, expected):
-        with open("config.yml", "r") as config_file: 
-            config = yaml.load(config_file, Loader=yaml.FullLoader)
-            assert config
-            assert config["experiment_name"] == expected
-            assert config["n_repeats"] > 0
-            assert type(config["scenario_params_list"]) == list
-            assert type(config["scenario_params_list"][0]) == dict
