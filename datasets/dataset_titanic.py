b1270d251555fa04fbae2710b870ea4a72dc1d82
aygalic <aygalic@hotmail.fr>
2020-07-17 13:17:16 +0200

    Enhancing model predictions
    Improved feature engineering

--- datasets/dataset_titanic.py
+++ datasets/dataset_titanic.py
@@ -5,13 +5,8 @@ Titanic dataset.
 """
 
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
-from keras.layers import Conv2D, MaxPooling2D
-import requests
 import pandas as pd
 import keras
-import os
 
 from sklearn.ensemble import RandomForestClassifier
 
@@ -37,6 +32,16 @@ def preprocess_dataset_inputs(x):
     """
 
     x["Sex"] = [i=="Male" for i in x["Sex"]]
+    x["Sex"] = [i=="Male" for i in x["Sex"]]
+
+    x['Title'] = [i.split()[0] for i in x["Name"]]
+    x = pd.concat([x, pd.get_dummies(x['Title'])], axis=1)
+
+    x['Name_Len'] = [len(i) for i in x["Name"]]
+
+    # Dropping the useless features
+    x.drop('Name', axis=1, inplace=True)
+    x.drop('Title', axis=1, inplace=True)
     return x.to_numpy()
 
 # download train and test sets
@@ -47,7 +52,7 @@ def load_data():
     """
 
     raw_dataset = pd.read_csv('https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv', index_col=False )
-    x = raw_dataset.drop(['Name','Survived'], axis=1)
+    x = raw_dataset.drop('Survived', axis=1)
     x = preprocess_dataset_inputs(x)
     y = raw_dataset['Survived']
     y = y.to_numpy()
@@ -64,20 +69,12 @@ def generate_new_model_for_dataset():
     Source : https://www.kaggle.com/bananachips/titanic-with-keras
     """
 
-    model = Sequential()
-    model.add(Dense(256,activation="relu", input_shape=input_shape))
-    model.add(keras.layers.core.Dropout(0.3))
-    model.add(Dense(128,activation="relu"))
-    model.add(keras.layers.core.Dropout(0.2))
-    model.add(Dense(64,activation="relu"))
-    model.add(Dense(2,activation="softmax"))
-    model.compile(
-        loss="categorical_crossentropy",
-        optimizer="adam",
-        metrics=["accuracy"]
-    )
-    clf = RandomForestClassifier(n_estimators=10)
-    return (model, clf)[1]
+
+    return RandomForestClassifier(criterion='gini',
+                                 n_estimators=700,
+                                 min_samples_leaf=1,
+                                 max_features='auto',
+                                 n_jobs=-1)
 
 # Load data
 (x_train, y_train), (x_test, y_test) = load_data()
