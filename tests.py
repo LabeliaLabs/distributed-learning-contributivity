3988f1933a13a3cad3dac527f9a7414cc560de9b
GauthierGar <gauthiergarioud@yahoo.fr>
2020-07-17 16:22:49 +0200

    test corrupt_labels

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
-# Pytest doc: https://docs.pytest.org/en/latest/getting-started.html#create-your-first-test
+# Pytest doc: https://docs.pytest.org/en/latest/getting-started.html
+#create-your-first-test
 # pytest tests.py
 # pytest -k TestDemoClass tests.py
 # pytest -k "test_ok" tests.py
@@ -7,6 +8,43 @@
 import utils
 import yaml
 
+import pytest
+
+import numpy as np
+
+from datasets import dataset_cifar10 as cf10
+from partner import Partner
+# ici rassembler bouts de code pour creer une liste
+# de scenario (config!) dont on se servira dans les tests
+
+class Test_partner:
+
+    def test_corrupt_labels_type(self):
+        with pytest.raises(TypeError):
+            part = Partner(partner_id=0)
+            part.corrupt_labels(part)
+
+    def test_corrupt_labels_type_elem(self):
+        with pytest.raises(TypeError):
+            part = Partner(partner_id=0)
+            (x_train, y_train), (x_test, y_test) = cf10.cifar10.load_data()
+            part.y_train = cf10.preprocess_dataset_labels(y_train)
+            part.y_train = part.y_train.astype("float64")
+            part.corrupt_labels(part)
+
+class Test_dataset_cifar10:
+
+    def test_preprocess_dataset_inputs_type(self):
+        x = cf10.preprocess_dataset_inputs(np.arange(20.))
+        assert x.dtype == "float32"
+
+    def test_preprocess_dataset_inputs_activation(self):
+        x= cf10.preprocess_dataset_inputs(np.arange(20.))
+        assert all( c <= 1 and c >= 0 for c in x)
+
+    def test_inputs_shape(self):
+        (x_train, y_train), (x_test, y_test) = cf10.cifar10.load_data()
+        assert x_train.shape[1:] == cf10.input_shape
 
 class TestDemoClass:
 
