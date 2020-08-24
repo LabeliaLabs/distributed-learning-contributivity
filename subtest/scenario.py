d33d42a95569e6db3af6ef93480f0069891ac24f
aygalic <aygalic@hotmail.fr>
2020-08-24 19:01:03 +0200

    Fix partner_list initialization

--- subtest/scenario.py
+++ subtest/scenario.py
@@ -29,6 +29,9 @@ class Scenario:
         # Initialization of the dataset defined in the config of the experiment
         # ---------------------------------------------------------------------
 
+        # List of all partners defined in the scenario
+        self.partners_list = []
+
         # Raise Exception if unknown parameters in the .yml file
 
         params_known = ["dataset_name", "dataset_proportion"]  # Dataset related
@@ -98,9 +101,6 @@ class Scenario:
         # --------------------------------------
         #  Definition of collaborative scenarios
         # --------------------------------------
-
-        # List of all partners defined in the scenario
-        self.partners_list = []
 
         # partners mock different partners in a collaborative data science project
         # For defining the number of partners
