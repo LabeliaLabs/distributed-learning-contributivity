db4896cdb6c49a91369c27eb6ff9d578ed954ad3
Romain Goussault <romain.goussault@gmail.com>
2020-08-27 20:53:37 +0200

    add csv reading

--- tests.py
+++ tests.py
@@ -417,15 +417,19 @@ class Test_EndToEndTest:
         # run test
         subprocess.run(["python", "main.py", "-f", "config_end_to_end_test.yml"])
 
-        # read results from csv
-        # df = pd.read_csv(...)
-        
-        # Extract score 
-        #test_score = df["test_score"]
-        test_score = 0.96
+        # Get latest generated folder
+        root_folder = Path().absolute().parent / "experiments" 
 
-        # clean up
+        subfolder_list = list(root_folder.glob('*end_to_end_test*'))
+        subfolder_list_creation_time = [f.stat().st_ctime for f in subfolder_list]
+        latest_subfolder_idx =  subfolder_list_creation_time.index(max(subfolder_list_creation_time))
+        experiment_path = subfolder_list[latest_subfolder_idx]
+        df = pd.read_csv(experiment_path / "results.csv")
+
+        # Extract score 
+        min_test_score = df["mpl_test_score"].min()
+        test_score = 0.96
         
         # assert  
-        assert  test_score > 0.95
+        assert  min_test_score > 0.96
         
