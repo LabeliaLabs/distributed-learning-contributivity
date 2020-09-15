4aa10a3c6c54dde78f71b15c5780ca4e1ceb2d1c
arthurPignet <arthur.pignet@mines-paristech.fr>
2020-09-15 18:33:11 +0200

    Change scenario.__init__ parameters

--- subtest/constants.py
+++ subtest/constants.py
@@ -8,7 +8,8 @@ DEFAULT_BATCH_SIZE = 256
 MAX_BATCH_SIZE = 2 ** 20
 DEFAULT_GRADIENT_UPDATES_PER_PASS_COUNT = 8
 PATIENCE = 10  # patience for early stopping
-
+DEFAULT_BATCH_COUNT = 20
+DEFAULT_EPOCH_COUNT = 40
 # GPU
 GPU_MEMORY_LIMIT_MB = 4096
 
@@ -20,7 +21,17 @@ DEBUG_LOGGING_FILE_NAME = "debug.log"
 EXPERIMENTS_FOLDER_NAME = "experiments"
 
 # Contributivity methods names
-# TODO
+CONTRIBUTIVITY_METHODS = [
+    "Shapley values",
+    "Independent scores",
+    "TMCS",
+    "ITMCS",
+    "IS_lin_S",
+    "IS_reg_S",
+    "AIS_Kriging_S",
+    "SMCS",
+    "WR_SMC",
+]
 
 # Datasets' Tags
 MNIST = "mnist"
@@ -33,3 +44,11 @@ SUPPORTED_DATASETS_NAMES = [MNIST, CIFAR10, TITANIC, ESC50]
 
 # Number of attempts allowed before raising an error while trying to download dataset
 NUMBER_OF_DOWNLOAD_ATTEMPTS = 3
+
+# Supported multipartner learning approaches
+MULTI_PARTNER_LEARNING_APPROACHES = [
+    "fedavg",
+    "seq-pure",
+    "seq-with-final-agg",
+    "seqavg",
+]
