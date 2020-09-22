5ee732d2180cff3528119ea4369b0243247a599f
arthurPignet <arthur.pignet@mines-paristech.fr>
2020-09-22 16:26:37 +0200

    New model, new preprocessing need to be tested
    Signed-off-by: arthurPignet <arthur.pignet@mines-paristech.fr>

--- mplc/datasets/dataset_imdb.py
+++ mplc/datasets/dataset_imdb.py
@@ -3,22 +3,25 @@ from time import sleep
 from urllib.error import HTTPError, URLError
 
 import numpy as np
-import tensorflow as tf
-from keras import layers
 from keras.datasets import imdb
+from keras.layers import Embedding, Conv1D, MaxPooling1D, Dense, Flatten, Dropout
 from keras.models import Sequential
-# Load Data
+from keras.preprocessing import sequence
 from loguru import logger
 from sklearn.model_selection import train_test_split
 
 from mplc import dataset, constants
+
+num_words = 5000
+input_shape = 500
+num_classes = 2
 
 
 def generate_new_dataset():
     attempts = 0
     while True:
         try:
-            (x_train, y_train), (x_test, y_test) = imdb.load_data()
+            (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=num_words)
             break
         except (HTTPError, URLError) as e:
             if hasattr(e, 'code'):
@@ -33,14 +36,9 @@ def generate_new_dataset():
                 attempts += 1
             else:
                 raise
-
-    input_shape = tf.keras.Input(shape=(None,), dtype="int64")
-    max_features = 20000
-    embedding_dim = 128
-    sequence_length = 500
-
-    # 2 Classes: review positive or negative
-    num_classes = 2
+    x_train, x_test, y_train, y_test = train_test_split(np.concatenate((x_train, x_test), axis=0),
+                                                        np.concatenate((y_train, y_test), axis=0),
+                                                        test_size=0.2)
 
     # Pre-process inputs
     x_train = preprocess_dataset_inputs(x_train)
@@ -66,45 +64,35 @@ def generate_new_dataset():
 
 
 def preprocess_dataset_labels(y):
-    y = tf.keras.utils.to_categorical(y, num_classes)
+    # vanilla label
     return y
+
+
+def preprocess_dataset_inputs(x):
+    x = sequence.pad_sequences(x, maxlen=input_shape)
+    return x
 
 
 # Model structure and generation
 def generate_new_model_for_dataset():
     """ Return a CNN model from scratch based on given batch_size"""
 
-    # Sequential groups a linear stack of layer into a tf.kera.Model
-    model = Sequential()  # Instancie un objet de type Sequential
+    model = Sequential()
+    model.add(Embedding(num_words, 32, input_length=input_shape))
+    model.add(Conv1D(filters=32,
+                     kernel_size=3,
+                     padding='same',
+                     activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-    # Next, we add a layer to map those vocab indices into a 
-    # space of dimensionality 'embedding_dim'.
-    # model.add(Embedding(max_features, embedding_dim))
-    # model.add(Dropout(0.5))
-
-    # Conv1D + global max pooling
-    # model.add(Conv1D(128, 7, padding="valid", activation="relu", strides=3))
-    # model.add(Conv1D(128, 7, padding="valid", activation="relu", strides=3))
-    # model.add(GlobalMaxPool1D())
-
-    # We add a vanilla hidden layer:
-    # model.add(Dense(128, activation="relu"))
-    # model.add(Dropout(0.5))
-
-    # model.add(Dense(1, activation="sigmoid"))
-
-    # Input - Layer
-    model.add(layers.Dense(50, activation="relu", input_shape=input_shape))
-    # Hidden - Layers
-    model.add(layers.Dropout(0.3, noise_shape=None, seed=None))
-    model.add(layers.Dense(50, activation="relu"))
-    model.add(layers.Dropout(0.2, noise_shape=None, seed=None))
-    model.add(layers.Dense(50, activation="relu"))
-    # Output- Layer
-    model.add(layers.Dense(1, activation="sigmoid"))
-
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer="adam",
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
                   metrics=['accuracy'])
 
     return model
