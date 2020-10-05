c58aebb13e30e68e14061c96e3930172867828fa
Romain Goussault <romain.goussault@gmail.com>
2020-10-05 19:37:10 +0200

    test contrib now working

--- tests/end_to_end_tests.py
+++ tests/end_to_end_tests.py
@@ -14,36 +14,6 @@ import constants  # noqa: E402
 
 class Test_EndToEndTest:
 
-    def test_mnist(self):
-        """
-        Test performance on MNIST dataset after one epoch
-        """
-        # run test
-        subprocess.run(["python", "main.py", "-f", "tests/config_end_to_end_test_mnist.yml"])
-
-        df = get_latest_dataframe("*end_to_end_test*"):
-
-        # Extract score
-        min_test_score = df["mpl_test_score"].min()
-
-        assert min_test_score > 0.95
-
-
-    def test_contrib(self):
-        """
-        Test contrib score
-        """
-        # run test
-        subprocess.run(["python", "main.py", "-f", "tests/config_end_to_end_test_contrib.yml"])
-
-        df = get_latest_dataframe("*end_to_end_test*"):
-
-        # Extract score
-        min_test_score = df["mpl_test_score"].min()
-
-        assert min_test_score > 0.95
-
-
     @staticmethod
     def get_latest_dataframe(pattern):
 
@@ -56,3 +26,38 @@ class Test_EndToEndTest:
         experiment_path = subfolder_list[latest_subfolder_idx]
 
         return pd.read_csv(experiment_path / "results.csv")
+
+    def test_mnist(self):
+        """
+        Test performance on MNIST dataset after one epoch
+        """
+        # run test
+        subprocess.run(["python", "main.py", "-f", "tests/config_end_to_end_test_mnist.yml"])
+
+        df = Test_EndToEndTest.get_latest_dataframe("*end_to_end_test*")
+
+        # Extract score
+        min_test_score = df["mpl_test_score"].min()
+
+        assert min_test_score > 0.95
+
+    def test_contrib(self):
+        """
+        Test contrib score
+        """
+        # run test
+        subprocess.run(["python", "main.py", "-f", "tests/config_end_to_end_test_contrib.yml"])
+
+        df = Test_EndToEndTest.get_latest_dataframe("*end_to_end_test*")
+
+        # 2 contributivity methods for each partner --> 4 lines
+        assert len(df) == 4
+
+        for contributivity_method in df.contributivity_method.unique():
+
+            current_df = df[df.contributivity_method == contributivity_method]
+
+            small_dataset_score = current_df.loc[current_df.dataset_fraction_of_partner == 0.1, "contributivity_score"]
+            big_dataset_score = current_df.loc[current_df.dataset_fraction_of_partner == 0.9, "contributivity_score"]
+
+            assert small_dataset_score.values < big_dataset_score.values
