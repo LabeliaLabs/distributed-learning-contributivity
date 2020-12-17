4d4141bd0244bd26a73c4cf0ae49e5ce1fca549c
Romain Goussault <romain.goussault@gmail.com>
2020-12-17 11:34:18 +0100

    fix tests

--- tests/contrib_end_to_end_test.py
+++ tests/contrib_end_to_end_test.py
@@ -3,8 +3,9 @@
 This enables to parameterize end to end tests - the tests are run by Travis each time you commit to the github repo
 """
 
-import subprocess
-from mplc import scenario
+from mplc.experiment import Experiment
+from mplc.scenario import Scenario
+from mplc.experiment import init_experiment_from_config_file
 
 from . import test_utils
 
@@ -15,9 +16,12 @@ class Test_EndToEndTest:
         """
         Test contributivity score on titanic dataset
         """
-        titanic_scenario = scenario.Scenario(2, [0.2, 0.8], epoch_count=3, minibatch_count=1, dataset_name='titanic',
-                                             contributivity_methods=["Federated SBS linear", "Shapley values"])
-        titanic_scenario.run()
+
+        titanic_scenario = Scenario(2, [0.1, 0.9], epoch_count=3, minibatch_count=1, dataset_name='titanic',
+                                    contributivity_methods=["Federated SBS linear", "Shapley values"])
+        exp = Experiment(experiment_name='end_to_end_test_contrib_titanic', nb_repeat=1,
+                         scenarios_list=[titanic_scenario])
+        exp.run()
 
         df = test_utils.get_latest_dataframe("*end_to_end_test*")
 
@@ -28,8 +32,8 @@ class Test_EndToEndTest:
 
             current_df = df[df.contributivity_method == contributivity_method]
 
-            small_dataset_score = current_df.loc[current_df.dataset_fraction_of_partner == 0.2, "contributivity_score"]
-            big_dataset_score = current_df.loc[current_df.dataset_fraction_of_partner == 0.8, "contributivity_score"]
+            small_dataset_score = current_df.loc[current_df.dataset_fraction_of_partner == 0.1, "contributivity_score"]
+            big_dataset_score = current_df.loc[current_df.dataset_fraction_of_partner == 0.9, "contributivity_score"]
 
             assert small_dataset_score.values < big_dataset_score.values
 
@@ -39,7 +43,8 @@ class Test_EndToEndTest:
         """
 
         # run test from config file
-        subprocess.run(["python", "main.py", "-f", "tests/config_end_to_end_test_contrib.yml"])
+        experiment = init_experiment_from_config_file("tests/config_end_to_end_test_contrib.yml")
+        experiment.run()
 
         df = test_utils.get_latest_dataframe("*end_to_end_test*")
 
