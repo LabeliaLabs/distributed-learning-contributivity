11eb20b32e9941e314f285c6451966f3027ffa9b
Amine Saboni <asaboni@octo.com>
2021-04-01 14:59:27 +0200

    feat: integrate tox to automate unit tests execution, setup.py refacto

--- setup.py
+++ setup.py
@@ -3,8 +3,30 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("mplc/requirements.txt", "r") as fh:
-    requirements = fh.read()
+requirements = [
+    'h5py==2.10.0',
+    'joblib==1.0.0',
+    'librosa==0.8.0',
+    'matplotlib==3.3.3',
+    'numpy==1.19.4',
+    'scipy==1.5.4',
+    'scikit-learn==0.23.2',
+    'pandas==1.1.5',
+    'seaborn==0.11.0',
+    'loguru==0.5.3',
+    'tensorflow==2.4.1',
+    'ruamel.yaml==0.16.12',
+    'flake8==3.8.3',
+    'setuptools==50.3.0',
+    'wheel==0.35.1',
+    'twine==3.2.0'
+]
+
+test_requirements = [
+    'tox',
+    'pytest==6.2.2',
+    'pytest-cov==2.10.1',
+]
 
 setup(
     name="mplc",
@@ -28,5 +50,6 @@ setup(
               'distributed learning'],
     license='Apache 2.0',
     install_requires=requirements,
+    tests_require=test_requirements,
     python_requires='>=3.6',
 )
